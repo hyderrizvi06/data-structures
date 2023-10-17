3f1facd9cf9c819c0ad1a6ab45980f976709df90
Hyder Rizvi <hmrizvi@stu.naperville203.org>
2023-10-17 13:29:09 -0500

    Demo

--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,7 +5,19 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
-        LinkedList students new LinkedList();
+        LinkedList students = new LinkedList();
         students.addFirst("Preston");
+        students.addFirst("Kaushik");
+        students.addFirst("Guzal");
+        students.addFirst("Emma");
+
+        System.out.println(students);
+
+        ListIterator iterator = students.listIterator();
+        iterator.next();
+        iterator.add("Hyder");
+        iterator.next();
+        iterator.remove();
+        
     }
 }
